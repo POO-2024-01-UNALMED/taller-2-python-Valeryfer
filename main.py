768f89cb9d9266e9770e3129546425a727b0e570
Valeryfer <vafernandez@unal.edu.co>
2024-02-16 11:33:24 -0500

    Creación de clases
    No terminado

---
+++ main.py
@@ -0,0 +1,47 @@
+class Auto:
+    cantidadCreados = 0
+    def __init__(self, modelo, precio, asientos, marca, motor, registro):
+        self.modelo = modelo
+        self.precio = precio
+        self.asientos = asientos
+        self.marca = marca
+        self.motor = motor
+        self.registro = registro
+            
+    def cantidadAsientos(self):
+        cantidadAsientos = asientos
+        return cantidadAsientos
+
+    def verificarIntegridad(self):
+        if 
+        else:
+            print("Auto original")
+
+class Asiento:
+    def __init__(self, color, precio, registro):
+        self.color = color
+        self.precio = precio
+        self.registro = registro
+    
+    def cambiarColor(self, color):
+        if (color == "rojo") OR (color == "verde") OR (color == "amarillo") OR (color == "negro") OR (color == "blanco"):
+            self.color = color
+        else:
+            self.color = self.color
+
+class Motor:
+    def __init__(self, numeroCilindros, tipo, registro):
+        self.numeroCilindros = numeroCilindros
+        self.tipo = tipo
+        self.registro = registro
+        
+    def cambiarRegistro(self, registro):
+        self.registro = registro
+
+    def asignarTipo(self, tipo):
+        if (tipo == "electrico"):
+            self.tipo = tipo
+        elif (tipo == "gasolina"):
+            self.tipo = tipo
+        else:
+            self.tipo = self.tipo
